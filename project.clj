1ca5753b179dd5cd283df2893b69fc40bfbdc1a4
Daniel Estevez <daniel.ef@gmail.com>
2018-04-13 18:24:58 -0500

    Defines values for Manifest

--- project.clj
+++ project.clj
@@ -1,6 +1,14 @@
-(defproject prion "0.1.0-SNAPSHOT"
+(defproject cognicious/prion "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+
+  :profiles {:instrumentation
+           {:main cognicious.prion.core
+            :aot [cognicious.prion.core]
+            :manifest {"Premain-Class" "cognicious.prion.core"
+                       "Can-Redefine-Classes" "false"
+                       "Can-Retransform-Classes" "true"
+                       :uberjar-name "prion.jar"}}})
